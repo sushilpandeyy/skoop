a4b1ab5f34af98ba7d6b200c4b7faae785fcf472
Contactsushil <contact.sushilpandey@gmail.com>
2024-03-20 14:26:16 +0530

    Created Basic Routing

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -2,9 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Homepage from './Pages/Homepage.jsx'
+import News from './Pages/News.jsx';
+
+const router= createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App/>}>
+      <Route path='' element={<Homepage/>}/>
+      <Route path='news' element={<News/>}/>
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router}/>
+</React.StrictMode>
 )
