fa2c7261997c8795916dd96ea5d26d6b5998a064
Contactsushil <contact.sushilpandey@gmail.com>
2024-03-20 16:42:28 +0530

    Created Add Page & Login System
    Login Shifted to login page

--- Client/src/Pages/Login.jsx
+++ Client/src/Pages/Login.jsx
@@ -1,7 +1,66 @@
-import React from 'react'
 import '../App.css';
+import { useState, useEffect } from 'react';
+import { ethers } from 'ethers';
+import abi from '../abi/News.json';
 
 const Login = () => {
+
+    const [state, setState] = useState({
+        provider: null,
+        signer: null,
+        Contract: null
+      });
+      const [account, setAccount] = useState(null);
+      const [walletConnected, setWalletConnected] = useState(false);
+    
+      useEffect(() => {
+        const connectWallet = async () => {
+          try {
+            let { ethereum } = window;
+            if (!ethereum) {
+              throw new Error('MetaMask not found');
+            }
+    
+            // Check if the wallet is connected
+            if (ethereum.selectedAddress) {
+              setAccount(ethereum.selectedAddress);
+              setWalletConnected(true);
+            } else {
+              setWalletConnected(false);
+            }
+    
+            ethereum.on('accountsChanged', (accounts) => {
+              if (accounts.length > 0) {
+                setAccount(accounts[0]);
+                setWalletConnected(true);
+              } else {
+                setAccount(null);
+                setWalletConnected(false);
+              }
+            });
+    
+            const contractAddress = "0xc3cCab5689A162D1c4C35bBCd15B56E7Ccab7A85";
+            const contractABI = abi.abi;
+    
+            const provider = new ethers.providers.Web3Provider(ethereum); // Read the Blockchain
+            const signer = provider.getSigner(); // Write the Blockchain
+    
+            const contract = new ethers.Contract(
+              contractAddress,
+              contractABI,
+              signer
+            );
+    
+            setState({ provider, signer, Contract: contract }); // Update the state with the contract instance
+            console.log(contract);
+          } catch (error) {
+            console.log(error);
+          }
+        };
+    
+        connectWallet();
+      }, []);
+
   return (
     
           <main className="w-full h-screen flex flex-col items-center justify-center bg-gray-50 sm:px-4 dark">
@@ -54,6 +113,14 @@ const Login = () => {
                           >
                               Create account
                           </button>
+                          {walletConnected ? (
+        <p>Connected Wallet: {account}</p>
+      ) : (
+        <button onClick={() => window.ethereum.request({ method: 'eth_requestAccounts' })}>
+          <p>{account}</p>
+          Connect Wallet
+        </button>
+      )}
                       </form>
                       {/* <div className="mt-5">
                           <button className="w-full flex items-center justify-center gap-x-3 py-2.5 mt-5 border rounded-lg text-sm font-medium hover:bg-gray-50 duration-150 active:bg-gray-100">
