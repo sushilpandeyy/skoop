8c1998abc873e53a8f77bb138a6b38dcb081b84d
Contactsushil <contact.sushilpandey@gmail.com>
2024-03-21 07:29:01 +0530

    Rectified Image Issue

--- Client/src/Pages/Textnews.jsx
+++ Client/src/Pages/Textnews.jsx
@@ -71,15 +71,15 @@ const Textnews = () => {
     }
     template();
   },[])
-  
+
   return (
 
     <div className='ter'>
     <h1>{newsid['result']['title']}</h1> 
+    <img src={newsid["result"]["imgUrl"]}/>
     <h2>{newsid['result']['category']}</h2> 
     <h3>{newsid['result']['summary']}</h3> 
     <h3>{newsid['result']['description']}</h3> 
-    <img src='{newsid["result"]["imgUrl"]}'/>
 
     </div>
   )
